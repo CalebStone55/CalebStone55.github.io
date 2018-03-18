63b9f4dd6810bca7dde41dba06ef2c86346eb679
stone55 <mr.stone_7@hotmail.com>
2018-03-18 09:55:44 -0600

    Updated nav bar and index h2

--- assignments/weathersite/scripts/currentdate.js
+++
@@ -1,7 +0,0 @@
-function currentDate() {
-	var n =  new Date();
-	var y = n.getFullYear();
-	var m = n.getMonth() + 1;
-	var d = n.getDate();
-	document.getElementById("date").innerHTML = d + ", " + d + " " + m + " " + y;
-}
--- assignments/weathersite/scripts/hamburger.js
+++
@@ -1,4 +0,0 @@
-function toggleNavMenu() {
-    console.log(document.getElementById("NavMenu").classList);
-	document.getElementsById("navMenu").classList.toggle("responsive");
-}
